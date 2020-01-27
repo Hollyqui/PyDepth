5d8d20afcf926eae8f9918233ec77bf332ecd692
Jonas Tjepkema <jonas@tjepkema.com>
2020-01-27 16:13:17 +0100

    Update PyDepth_Run.bat

--- PyDepth_Streaming/PyDepth_Run.bat
+++ PyDepth_Streaming/PyDepth_Run.bat
@@ -1,5 +1,8 @@
+@ECHO OFF
 ECHO You will be able to enter the password in 7 seconds.
 ECHO
 start /B python PyDepth_serverSocket.py
-SLEEP 7
+ECHO
+TIMEOUT 7
+ECHO
 plink pi@pydepth.local python ~/PyDepth_Client/PyDepth_clientSocket.py
